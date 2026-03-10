d0ac3b603b7440c48dc42b62bb126fcb25a3f29e
이강복 <kangboklee@genians.com>
2026-03-10 11:22:34 +0900

    Update ubuntunac_install.sh

--- ubuntunac_install.sh
+++ ubuntunac_install.sh
@@ -51,6 +51,7 @@ systemctl stop unattended-upgrades.service > /dev/null 2>&1
 systemctl disable unattended-upgrades.service > /dev/null 2>&1
 systemctl stop apt-daily.timer > /dev/null 2>&1
 systemctl disable apt-daily.timer > /dev/null 2>&1
+util::info "Remove unattended upgrade service"
 rm -rf /var/lib/dpkg/lock-frontend > /dev/null 2>&1
 rm -rf /var/lib/apt/lists/lock > /dev/null 2>&1
 rm -rf /var/cache/apt/archives/lock > /dev/null 2>&1
